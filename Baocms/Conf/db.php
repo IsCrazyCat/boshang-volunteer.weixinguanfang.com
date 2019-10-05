360003cd430c3e997a4a87fbc5699ddd9c995dc7
guojunguang <505088726@qq.com>
2019-10-05 14:46:41 +0800

    初始代码提交

---
+++ Baocms/Conf/db.php
@@ -0,0 +1,15 @@
+<?php
+
+    return  array(
+    'DB_TYPE'   =>  'mysql',
+    'DB_HOST'   =>  '127.0.0.1',
+    'DB_NAME'   =>  'volunteer_weixinguanfang_com',//数据库名字，不要用记事本修改
+    'DB_USER'   =>  'volunteer',//数据库用户名，不要用记事本修改
+    'DB_PWD'    =>  'd89b590388b27f4',//数据库密码，不要用记事本修改
+    'DB_PORT'   =>   3306 ,
+    'DB_CHARSET'=>  'utf8',
+    'DB_PREFIX' =>  'bao_',
+    'AUTH_KEY'  =>  '520efebc109577cc0a86de013d0164ac', //这个KEY只是保证部分表单在没有SESSION 的情况下判断用户本人操作的作用
+    'BAO_KEY'   => '',
+
+);
